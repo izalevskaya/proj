02908383036576f3450d1fb2040364bb8f3550e2
Irina Zalevskaya <zalevskaya1988@gmail.com>
2026-02-04 14:04:43 +0600

    улучшили логику1

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -10,7 +10,7 @@ public class MainApplication {
     }
 
     public static void main(String[] args) {
-        System.out.println("0");
+        System.out.println("1");
         System.out.println("0");
         System.out.println("0");
     }
