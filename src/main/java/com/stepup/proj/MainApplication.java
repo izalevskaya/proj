50758b11c0f031984634f076d556daacc8929455
Irina Zalevskaya <zalevskaya1988@gmail.com>
2026-02-04 14:03:58 +0600

    улучшили логику

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,15 +3,15 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("11011");
+        System.out.println("1----");
         System.out.println("22222");
         System.out.println("33333");
 
     }
 
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("0");
+        System.out.println("0");
+        System.out.println("0");
     }
 }
