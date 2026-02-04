326988c97d26a8de5ee4403f2144f1267eded79b
Irina Zalevskaya <zalevskaya1988@gmail.com>
2026-02-04 13:53:59 +0600

    New правки влогике

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -8,7 +8,10 @@ public class MainApplication {
         System.out.println("33333");
 
     }
+
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
