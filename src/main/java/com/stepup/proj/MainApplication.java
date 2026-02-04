d24787a7c28897304c10ac5b668f9640acfce0a7
Irina Zalevskaya <zalevskaya1988@gmail.com>
2026-02-04 14:00:08 +0600

    изменили параметры

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,13 +5,13 @@ public class MainApplication {
         System.out.println("Hello World!");
         System.out.println("11011");
         System.out.println("22222");
-        System.out.println("33333");
+        System.out.println("3-----");
 
     }
 
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("0");
+        System.out.println("0");
     }
 }
